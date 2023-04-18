3c2b5d69f7de253b13bca8adaeeab952a6199b81
Hasan Malik <hmalik0412@gmail.com>
2023-04-18 15:46:01 +0100

    Created arrays in JavaScript

--- Develop/script.js
+++ Develop/script.js
@@ -17,3 +17,10 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 window.alert("Your password must contain between 8 characters and 128 characters. ");
+
+const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const specialcharacters = ["!", "£", "#", "$", "%", "^", "&", "*", ":", ";", "-", "=", "+", "."];
+
+
