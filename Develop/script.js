edc4be6c8b466c7599d903b815fa848cfa8b078b
Hasan Malik <hmalik0412@gmail.com>
2023-04-20 15:57:49 +0100

    Changed confirm messages to variables

--- Develop/script.js
+++ Develop/script.js
@@ -9,7 +9,7 @@ function writePassword() {
   passwordText.value = password;
 }
 
-window.alert("WELCOME TO MALIK PASSWORD GENERATOR! ");
+window.alert("WELCOME TO MALIK PASSWORD GENERATOR!  ");
 
 function generatePassword() {
   var passwordLength = parseInt(prompt("YOUR PASSWORD MUST CONTAIN BETWEEN 8 CHARACTERS AND 128 CHARACTERS! ")); 
@@ -26,15 +26,18 @@ const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const number = "0123456789";
 const specialCharacters = "!@£$%^&*()_+{}:|?><~€#-=[];'\/.,`";
 
-const includeLowercase = confirm("WOULD YOU LIKE THE PASSWORD TO INCLUDE LOWERCASE LETTERS?");
-const includeUppercase = confirm("WOULD YOU LIKE THE PASSWORD TO INCLUDE UPPERCASE LETTERS?");
-const includeNumber = confirm("WOULD YOU LIKE THE PASSWORD TO INCLUDE NUMBERS?");
-const includeSpecialCharacters = confirm("WOULD YOU LIKE THE PASSWORD TO INCLUDE SPECIAL CHARACTERS?");
+var includeLowercase = confirm("WOULD YOU LIKE THE PASSWORD TO INCLUDE LOWERCASE LETTERS?");
+var includeUppercase = confirm("WOULD YOU LIKE THE PASSWORD TO INCLUDE UPPERCASE LETTERS?");
+var includeNumber = confirm("WOULD YOU LIKE THE PASSWORD TO INCLUDE NUMBERS?");
+var includeSpecialCharacters = confirm("WOULD YOU LIKE THE PASSWORD TO INCLUDE SPECIAL CHARACTERS?");
 
 if (!includeLowercase && !includeUppercase && !includeNumber && !includeSpecialCharacters) {
   alert("ERROR! AT LEASE ONE CHARACTER TYPE MUST BE SELECTED.");
   return;
 }
+
+var message = "";
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
