6c0bed7fb84b933479bffb83a0957d1b854cbb62
Hasan Malik <hmalik0412@gmail.com>
2023-04-20 15:24:59 +0100

    Confirm function added with message

--- Develop/script.js
+++ Develop/script.js
@@ -14,10 +14,10 @@ window.alert("Your password must contain between 8 characters and 128 characters
 function generatePassword() {
   var passwordLength = parseInt(prompt("YOUR PASSWORD MUST CONTAIN BETWEEN 8 CHARACTERS AND 128 CHARACTERS! ")); 
   if (passwordLength < 8 | !Number.isInteger(passwordLength)) {
-    alert(" PASSWORD IS TOO SHORT, IT MUST CONTAIN AT LEAST 8 CHARACTERS. ");
+    alert("PASSWORD IS TOO SHORT, IT MUST CONTAIN AT LEAST 8 CHARACTERS. ");
     return; }
   if (passwordLength > 128 | !Number.isInteger(passwordLength)) {
-    alert(" PASSWORD IS TOO LONG, IT MUST CONTAIN A MAXIMUM OF 128 CHARACTERS. ");
+    alert("PASSWORD IS TOO LONG, IT MUST CONTAIN A MAXIMUM OF 128 CHARACTERS. ");
     return; }
   }
 
@@ -26,8 +26,10 @@ const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const number = "0123456789";
 const specialCharacters = "!@£$%^&*()_+{}:|?><~€#-=[];'\/.,`";
 
-
-
+const includeLowercase = confirm("WOULD YOU LIKE THE PASSWORD TO INCLUDE LOWERCASE LETTERS?");
+const includeUppercase = confirm("WOULD YOU LIKE THE PASSWORD TO INCLUDE UPPERCASE LETTERS?");
+const includeNumber = confirm("WOULD YOU LIKE THE PASSWORD TO INCLUDE NUMBERS?");
+const includeSpecialCharacters = confirm("WOULD YOU LIKE THE PASSWORD TO INCLUDE SPECIAL CHARACTERS?");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
